e10cf56ad10917d71605244ca45f951330a1ae0f
JoaoCarlos0209 <joaocardoso0209@gmail.com>
2024-06-17 22:43:36 -0300

    Aula1706

---
+++ src/test/java/aula1706/exercicio/CalculadoraTeste/CalculadoraTeste.java
@@ -0,0 +1,42 @@
+package aula1706.exercicio.CalculadoraTeste;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import aula1706.exercicio.Calculadora.Calculadora;
+
+public class CalculadoraTeste {
+    
+    @Test
+    public void testSoma() {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.soma(2, 3);
+        assertEquals(5 , resultado);
+        System.out.println("O resultado da soma é: " + resultado);
+    }
+
+    @Test
+    public void testSubtracao() {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.subtracao(4, 3);
+        assertEquals(1 , resultado);
+        System.out.println("O resultado da subtracao é: " + resultado);
+    }
+
+    @Test
+    public void testMultiplicao() {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.multiplicacao(2, 3);
+        assertEquals(6 , resultado);
+        System.out.println("O resultado da multiplicação é: " + resultado);
+    }
+
+    @Test
+    public void testDivisao() {
+        Calculadora calculadora = new Calculadora();
+        int resultado = calculadora.divisao(6, 3);
+        assertEquals(2 , resultado);
+        System.out.println("O resultado da divisão é: " + resultado);
+    }
+}
